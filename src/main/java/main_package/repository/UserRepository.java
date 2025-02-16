cea724eda5a3d091a15088ecedaef936c982ca5a
Бабин Даниил Алексеевич <113771449+goder1@users.noreply.github.com>
2025-02-16 22:19:52 +0300

    Revert "First homework done"
    This reverts commit 1575917374185441d0fc2026f4777a548ee0cf56.

--- src/main/java/main_package/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package main_package.repository;
-
-import main_package.entity.UserData;
-
-public interface UserRepository {
-  UserData getUserDataById(Long id);
-
-  Long createUser(UserData user);
-}
--- src/main/java/main_package/response/UniversityGetResponse.java
+++
@@ -1,4 +0,0 @@
-package main_package.response;
-
-public record UniversityGetResponse(String name, Long students, String location) {
-}
